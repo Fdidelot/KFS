10a50578a66e82274f5084dabaeebf7e4feb07ca
melperri <melperri@student.42.fr>
2025-10-18 18:01:05 +0200

    handle_command return eax

--- KFS-2/srcs/handle_command.asm
+++ KFS-2/srcs/handle_command.asm
@@ -1,3 +1,4 @@
+extern print_enter
 extern ft_strcmp
 extern help
 extern print_registers
@@ -60,11 +61,15 @@ handle_command:
     jmp .loop
 
 .found:
+	call print_enter
     pop ebx                 ; handler table
     pop esi                 ; restore command table
     mov eax, [ebx]          ; eax = fonction associée
     call eax                ; call fonction
-    jmp .done
+	xor eax, eax
+	ret
+    ;jmp .done
 
 .done:
+	mov eax, -1
     ret
